2c2911a2cece2d7334b3264431104ed40a76238d
Matt Clarkson <m.clarkson@ucl.ac.uk>
2016-02-05 17:34:56 +0000

    Updated detector and optimiser interface

---
+++ Code/include/niftkIDetector.h
@@ -0,0 +1,34 @@
+/*=============================================================================
+
+  NiftyCal: A software package for camera calibration.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#ifndef niftkIDetector_h
+#define niftkIDetector_h
+
+namespace niftk
+{
+
+/**
+* \brief Interface for anything that detects points in an image.
+*/
+class IDetector
+{
+
+public:
+
+
+};
+
+} // end namespace
+
+#endif
