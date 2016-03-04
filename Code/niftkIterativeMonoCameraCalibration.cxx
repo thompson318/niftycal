bf1669b5a4529566e6699c2aa6fdd6ee91701e5f
Matt Clarkson <m.clarkson@ucl.ac.uk>
2016-03-04 10:24:36 +0000

    Added Eddie sim1 and sim2

--- Code/niftkIterativeMonoCameraCalibration.cxx
+++ Code/niftkIterativeMonoCameraCalibration.cxx
@@ -152,6 +152,7 @@ double IterativeMonoCameraCalibration(
       cp = (*canonicalIter).first->GetPoints();
       if(cp.empty())
       {
+        cv::imwrite("/tmp/matt.jpg", (*canonicalIter).second);
         niftkNiftyCalThrow() << "All warped images should still contain valid calibration images containing extractable points.";
       }
 
