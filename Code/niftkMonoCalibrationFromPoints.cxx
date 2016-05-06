6fca8fe23de38726abe212a8ae4d578b27e831a8
Matt Clarkson <m.clarkson@ucl.ac.uk>
2016-05-06 07:46:07 +0100

    Issue #1: Actually add new files

---
+++ Code/niftkMonoCalibrationFromPoints.cxx
@@ -0,0 +1,76 @@
+/*=============================================================================
+
+  NiftyCal: A software package for camera calibration.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#include "niftkIOUtilities.h"
+#include "niftkMonoCameraCalibration.h"
+#include <cv.h>
+#include <list>
+#include <cstdlib>
+
+/**
+ * \brief Calibrate mono camera, using pre-extracted points,
+ * using niftk::MonoCameraCalibration routine.
+ */
+int main(int argc, char ** argv)
+{
+  if (argc < 6)
+  {
+    std::cerr << "Usage: niftkMonoCalibrationFromPoints imageSizeX imageSizeY modelPoints.txt "
+              << "imagePoints1.txt imagePoints2.txt ... imagePointsN.txt" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  int sizeX = atoi(argv[1]);
+  int sizeY = atoi(argv[2]);
+  std::string modelFile = argv[3];
+
+  cv::Size2i imageSize(sizeX, sizeY);
+  niftk::Model3D model = niftk::LoadModel3D(modelFile);
+  std::list<niftk::PointSet> points;
+
+  for (int i = 4; i < argc; i++)
+  {
+    niftk::PointSet p = niftk::LoadPointSet(argv[i]);
+    points.push_back(p);
+  }
+
+  cv::Mat intrinsic;
+  cv::Mat distortion;
+  std::vector<cv::Mat> rvecs;
+  std::vector<cv::Mat> tvecs;
+
+  double rms = niftk::MonoCameraCalibration(model,
+                                            points,
+                                            imageSize,
+                                            intrinsic,
+                                            distortion,
+                                            rvecs,
+                                            tvecs
+                                            );
+
+  std::cout << "niftkMonoCalibrationFromPoints:(" << imageSize.width << "," << imageSize.height <<  ") "
+            << points.size() << " "
+            << intrinsic.at<double>(0,0) << " "
+            << intrinsic.at<double>(1,1) << " "
+            << intrinsic.at<double>(0,2) << " "
+            << intrinsic.at<double>(1,2) << " "
+            << distortion.at<double>(0,0) << " "
+            << distortion.at<double>(0,1) << " "
+            << distortion.at<double>(0,2) << " "
+            << distortion.at<double>(0,3) << " "
+            << rms
+            << std::endl;
+
+  return EXIT_SUCCESS;
+}
---
+++ Code/niftkStereoCalibrationFromPoints.cxx
@@ -0,0 +1,149 @@
+/*=============================================================================
+
+  NiftyCal: A software package for camera calibration.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#include "niftkIOUtilities.h"
+#include "niftkMonoCameraCalibration.h"
+#include "niftkStereoCameraCalibration.h"
+#include <cv.h>
+#include <list>
+#include <cstdlib>
+
+/**
+ * \brief Calibrate stereo camera, using pre-extracted points,
+ * using niftk::StereoCameraCalibration routine.
+ */
+int main(int argc, char ** argv)
+{
+  if (argc < 8)
+  {
+    std::cerr << "Usage: niftkStereoCalibrationFromPoints imageSizeX imageSizeY modelPoints.txt "
+              << "leftImagePoints1.txt leftImagePoints2.txt ... leftImagePointsN.txt "
+              << "rightImagePoints1.txt rightImagePoints2.txt ... rightImagePointsN.txt " << std::endl;
+
+    return EXIT_FAILURE;
+  }
+
+  int numberOfArgumentsBeforeImages = 4;
+  int numberOfImagesPerSide = (argc-numberOfArgumentsBeforeImages)/2;
+
+  if ((argc - numberOfArgumentsBeforeImages)%2 != 0)
+  {
+    std::cerr << "Expected an even number of point files" << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  int sizeX = atoi(argv[1]);
+  int sizeY = atoi(argv[2]);
+  std::string modelFile = argv[3];
+
+  cv::Size2i imageSize(sizeX, sizeY);
+  niftk::Model3D model = niftk::LoadModel3D(modelFile);
+  std::list<niftk::PointSet> leftPoints;
+  std::list<niftk::PointSet> rightPoints;
+
+  for (int i = numberOfArgumentsBeforeImages; i < argc; i++)
+  {
+    niftk::PointSet p = niftk::LoadPointSet(argv[i]);
+
+    if (i-numberOfArgumentsBeforeImages < numberOfImagesPerSide)
+    {
+      leftPoints.push_back(p);
+    }
+    else
+    {
+      rightPoints.push_back(p);
+    }
+  }
+
+  cv::Mat intrinsicLeft;
+  cv::Mat distortionLeft;
+  std::vector<cv::Mat> rvecsLeft;
+  std::vector<cv::Mat> tvecsLeft;
+
+  cv::Mat intrinsicRight;
+  cv::Mat distortionRight;
+  std::vector<cv::Mat> rvecsRight;
+  std::vector<cv::Mat> tvecsRight;
+
+  cv::Mat essentialMatrix = cvCreateMat (3,3,CV_64FC1);
+  cv::Mat fundamentalMatrix = cvCreateMat (3,3,CV_64FC1);
+  cv::Mat left2RightRotation = cvCreateMat (1,3,CV_64FC1);
+  cv::Mat left2RightTranslation = cvCreateMat (1,3,CV_64FC1);
+
+  niftk::MonoCameraCalibration(model,
+                               leftPoints,
+                               imageSize,
+                               intrinsicLeft,
+                               distortionLeft,
+                               rvecsLeft,
+                               tvecsLeft
+                              );
+
+  niftk::MonoCameraCalibration(model,
+                               rightPoints,
+                               imageSize,
+                               intrinsicRight,
+                               distortionRight,
+                               rvecsRight,
+                               tvecsRight
+                              );
+
+  double rms = niftk::StereoCameraCalibration(model,
+                                              leftPoints,
+                                              rightPoints,
+                                              imageSize,
+                                              intrinsicLeft,
+                                              distortionLeft,
+                                              rvecsLeft,
+                                              tvecsLeft,
+                                              intrinsicRight,
+                                              distortionRight,
+                                              rvecsRight,
+                                              tvecsRight,
+                                              left2RightRotation,
+                                              left2RightTranslation,
+                                              essentialMatrix,
+                                              fundamentalMatrix,
+                                              CV_CALIB_USE_INTRINSIC_GUESS
+                                             );
+
+  std::cout << "niftkStereoCalibrationFromPoints:(" << imageSize.width << "," << imageSize.height <<  ") "
+            << leftPoints.size() << " "
+            << intrinsicLeft.at<double>(0,0) << " "
+            << intrinsicLeft.at<double>(1,1) << " "
+            << intrinsicLeft.at<double>(0,2) << " "
+            << intrinsicLeft.at<double>(1,2) << " "
+            << distortionLeft.at<double>(0,0) << " "
+            << distortionLeft.at<double>(0,1) << " "
+            << distortionLeft.at<double>(0,2) << " "
+            << distortionLeft.at<double>(0,3) << " "
+            << intrinsicRight.at<double>(0,0) << " "
+            << intrinsicRight.at<double>(1,1) << " "
+            << intrinsicRight.at<double>(0,2) << " "
+            << intrinsicRight.at<double>(1,2) << " "
+            << distortionRight.at<double>(0,0) << " "
+            << distortionRight.at<double>(0,1) << " "
+            << distortionRight.at<double>(0,2) << " "
+            << distortionRight.at<double>(0,3) << " "
+            << left2RightRotation.at<double>(0,0) << " "
+            << left2RightRotation.at<double>(0,1) << " "
+            << left2RightRotation.at<double>(0,2) << " "
+            << left2RightTranslation.at<double>(0,0) << " "
+            << left2RightTranslation.at<double>(0,1) << " "
+            << left2RightTranslation.at<double>(0,2) << " "
+            << rms
+            << std::endl;
+
+  return EXIT_SUCCESS;
+}
