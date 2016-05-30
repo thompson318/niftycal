6bb4141eabb2e1e293bd96c98b4fc2001f1b8b97
Matt Clarkson <m.clarkson@ucl.ac.uk>
2016-05-30 21:46:51 +0100

    Issue #12: Call out to refactored template matching.

--- Code/niftkOpenCVRingsPointDetector.cxx
+++ Code/niftkOpenCVRingsPointDetector.cxx
@@ -16,6 +16,7 @@
 #include "niftkNiftyCalExceptionMacro.h"
 #include "niftkHomographyUtilities.h"
 #include "niftkMatrixUtilities.h"
+#include "niftkOpenCVTemplateMatching.h"
 #include <cv.h>
 #include <highgui.h>
 
@@ -42,11 +43,11 @@ OpenCVRingsPointDetector::OpenCVRingsPointDetector(cv::Size2i patternSize,
   {
     niftkNiftyCalThrow() << "Number of circles in height axes is < 2.";
   }
-  if (offsetForTemplate.width < 1)
+  if (m_OffsetForTemplate.width < 1)
   {
     niftkNiftyCalThrow() << "Offset width must be >= 1";
   }
-  if (offsetForTemplate.height < 1)
+  if (m_OffsetForTemplate.height < 1)
   {
     niftkNiftyCalThrow() << "Offset height must be >= 1";
   }
@@ -184,9 +185,6 @@ PointSet OpenCVRingsPointDetector::GetPointsUsingContours(const cv::Mat& image)
   cv::Mat littleBlobs; // inner circle
   this->ExtractBlobs(image, bigBlobs, littleBlobs);
 
-  cv::imwrite("/tmp/matt.little.png", littleBlobs);
-  cv::imwrite("/tmp/matt.big.png", bigBlobs);
-
   cv::SimpleBlobDetector::Params params;
   params.maxArea = m_MaxAreaInPixels;
   cv::Ptr<cv::FeatureDetector> blobDetector = new cv::SimpleBlobDetector(params);
@@ -230,8 +228,9 @@ PointSet OpenCVRingsPointDetector::GetPointsUsingContours(const cv::Mat& image)
 
 
 //-----------------------------------------------------------------------------
-PointSet OpenCVRingsPointDetector::GetPointsUsingTemplateMatching(
-    const cv::Mat& image, const niftk::PointSet& startingGuess)
+PointSet OpenCVRingsPointDetector::GetPointsUsingTemplateMatching(const cv::Mat& image,
+                                                                  const niftk::PointSet& startingGuess
+                                                                 )
 {
   niftk::PointSet result;
 
@@ -246,108 +245,22 @@ PointSet OpenCVRingsPointDetector::GetPointsUsingTemplateMatching(
     niftk::PointSet warpedPoints;
     niftk::WarpPointsByHomography(startingGuess, homography, warpedPoints);
 
-    niftk::PointSet matchedPoints = this->DoTemplateMatchingForAllPoints(warpedImage, *m_TemplateImage, warpedPoints);
+    niftk::PointSet matchedPoints = niftk::DoTemplateMatchingForAllPoints(warpedImage,
+                                                                          *m_TemplateImage,
+                                                                          m_OffsetForTemplate,
+                                                                          warpedPoints);
+
     niftk::WarpPointsByHomography(matchedPoints, homography.inv(), result);
   }
   else
   {
-    result = this->DoTemplateMatchingForAllPoints(image, *m_TemplateImage, startingGuess);
+    result = niftk::DoTemplateMatchingForAllPoints(image,
+                                                   *m_TemplateImage,
+                                                   m_OffsetForTemplate,
+                                                   startingGuess
+                                                  );
   }
   return result;
-}
-
-
-//-----------------------------------------------------------------------------
-PointSet OpenCVRingsPointDetector::DoTemplateMatchingForAllPoints(
-    const cv::Mat& image, const cv::Mat& templateImage, const niftk::PointSet& startingPoints)
-{
-  // This is the output.
-  niftk::PointSet updatedPoints;
-
-  cv::Size2i templateSize = templateImage.size();
-
-  cv::Point2d halfTemplateSize;
-  halfTemplateSize.x = (templateSize.width - 1)/2.0;
-  halfTemplateSize.y = (templateSize.height - 1)/2.0;
-
-  // Copy template into float image - once.
-  cv::Mat templateAsFloat = cvCreateMat(templateImage.cols, templateImage.rows, CV_32F);
-  templateImage.convertTo(templateAsFloat, CV_32F);
-
-  // This is to store a copy of the image data, as we need float.
-  cv::Mat regionAsFloat = cvCreateMat(2 * m_OffsetForTemplate.width + templateSize.width,
-                                      2 * m_OffsetForTemplate.height + templateSize.height,
-                                      CV_32F
-                                     );
-
-  // This is the results array, temporary storage, for output of template matching.
-  cv::Mat result = cvCreateMat(regionAsFloat.cols - templateAsFloat.cols + 1,
-                               regionAsFloat.rows - templateAsFloat.rows + 1,
-                               CV_32F
-                              );
-
-  cv::Point2d startingOffset;
-  startingOffset.x = m_OffsetForTemplate.width + halfTemplateSize.x;
-  startingOffset.y = m_OffsetForTemplate.height + halfTemplateSize.y;
-
-  double minVal = 0;
-  double maxVal = 0;
-  cv::Point minLoc;
-  cv::Point maxLoc;
-  cv::Point matchLoc;
-  cv::Point2d originalPoint;
-  cv::Point2d interpolatedPoint;
-  niftk::Point2D p;
-  cv::Matx33d surface;
-
-  niftk::PointSet::const_iterator iter;
-  for (iter  = startingPoints.begin();
-       iter != startingPoints.end();
-       ++iter
-       )
-  {
-    originalPoint = (*iter).second.point;
-    niftk::NiftyCalIdType id = (*iter).first;
-
-    cv::Rect rect(static_cast<int>(originalPoint.x - startingOffset.x),
-                  static_cast<int>(originalPoint.y - startingOffset.y),
-                  regionAsFloat.cols,
-                  regionAsFloat.rows
-                  );
-
-    cv::Mat region(image, rect);
-    region.convertTo(regionAsFloat, CV_32F);
-
-    cv::matchTemplate( regionAsFloat, templateAsFloat, result, cv::TM_CCOEFF_NORMED );
-    cv::minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, cv::Mat() );
-    matchLoc = maxLoc;
-
-    for (int r = 0; r < 3; r++)
-    {
-      for (int c = 0; c < 3; c++)
-      {
-        surface(r, c) = result.at<float>(matchLoc.y - 1 + r, matchLoc.x - 1 + c);
-      }
-    }
-    niftk::InterpolateMaximumOfQuadraticSurface(surface, interpolatedPoint);
-
-    p.id = id;
-    p.point.x = rect.x + matchLoc.x + interpolatedPoint.x + halfTemplateSize.x;
-    p.point.y = rect.y + matchLoc.y + interpolatedPoint.y + halfTemplateSize.y;
-
-    updatedPoints.insert(niftk::IdPoint2D(id, p));
-/*
-    std::cerr << "Size=" << updatedPoints.size()
-              << ", id=" << id
-              << ", ip=" << originalPoint
-              << ", op=" << p.point
-              << ", ml=" << matchLoc
-              << ", ip=" << interpolatedPoint
-              << ", r=" << rect
-              << std::endl;
-*/
-  }
-  return updatedPoints;
 }
 
 
