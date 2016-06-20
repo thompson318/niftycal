a33294a98daf87f5cd5c9ae8ebfe15dabc4cedcc
Matt Clarkson <m.clarkson@ucl.ac.uk>
2016-06-20 06:27:08 +0100

    Issue #23: Rename python functions to be more explicit.

--- Code/Python/niftk_file_utils.py
+++ Code/Python/niftk_file_utils.py
@@ -11,9 +11,18 @@ def number_of_lines(fname):
         raise IOError(err)
     return int(result.strip().split()[0])
 
-def get_files_by_name_and_line_count(dirName, ending, lineCount):
+def get_files_by_ending(dirName, ending):
     fileNames = []
     for f in os.listdir(dirName):
-        if f.endswith(ending) and number_of_lines(f) >= int(lineCount):
+        if f.endswith(ending):
             fileNames.append(f)
     return fileNames
+
+def get_files_by_ending_and_line_count(dirName, ending, lineCount):
+    fileNames = get_files_by_ending(dirName, ending)
+    longEnoughFiles = []
+    for f in fileNames:
+        if number_of_lines(f) >= int(lineCount):
+            longEnoughFiles.append(f)
+    return longEnoughFiles
+
