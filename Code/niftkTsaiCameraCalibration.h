b55f5b9d3b2f82c472669ea58502d30f5e63c7ff
Matt Clarkson <m.clarkson@ucl.ac.uk>
2016-11-25 08:52:56 +0000

    Issue #34: Tweak doxygen.

--- Code/niftkTsaiCameraCalibration.h
+++ Code/niftkTsaiCameraCalibration.h
@@ -53,6 +53,11 @@ NIFTYCAL_WINEXPORT double TsaiMonoCameraCalibration(const niftk::Model3D& model3
 * Will check for planarity and then switch to either the coplanar or the non-coplanar method.
 * This method computes Tsai 1987 for left and right camera, and then optimises
 * the full intrinsic (2 x 4 DOF), distortion (2 x 1 DOF) and extrinsic (2 x 6 DOF) = 22 DOF.
+*
+* NOTE: This method, ONLY does the stereo calibration, so if ITK is not compiled in,
+* then there is no non-linear optimisation, so all it does is compute the
+* right to left transformation.
+*
 * \param fullOptimisation defaults to false, to most closely match the paper.
 * \return rms re-projection error.
 */
