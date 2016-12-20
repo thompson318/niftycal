16848f43386ff3fca0a24405be4cbfa819d413cf
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2016-12-20 17:00:16 +0000

    Issue #39 crop images

--- Code/Bash/niftkMakeAprilTagGrid.bash
+++ Code/Bash/niftkMakeAprilTagGrid.bash
@@ -1,9 +1,20 @@
 #! /bin/bash
-
-GRIDFILE=/home/thompson/build/NifTK-qt4.8.7/AprilTags/src/tags/tag16h5.ps
+#we only need 24 tags for a chessboard equivalent grid, but we need more. if we're going to 
+#extend the grid beyond the image
+#GRIDFILE=/home/thompson/build/NifTK-qt4.8.7/AprilTags/src/tags/tag25h7.ps
+GRIDFILE=/home/thompson/work/build/NifTK-dbg/AprilTags/src/tags/tag25h7.ps
 OUTDIR=/dev/shm/AprilGridTemp
 mkdir -p $OUTDIR
 
 #we'll use imagemagick's convert
 
-convert $GRIDFILE $OUTDIR/tag.bmp
+#convert $GRIDFILE $OUTDIR/tag.bmp
+
+files=$(ls $OUTDIR)
+
+for file in $files 
+do
+	echo convert $file -crop 612x612+0+90 ${file#.*}_cropped.bmp
+done
+
+
