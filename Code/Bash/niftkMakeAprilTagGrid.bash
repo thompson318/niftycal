0a3b24db40a7fc116546e4560c286d1e9ec297bc
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2017-01-18 16:11:34 +0000

    Issue #39 tidied up AprilTag script

--- Code/Bash/niftkMakeAprilTagGrid.bash
+++ Code/Bash/niftkMakeAprilTagGrid.bash
@@ -1,21 +1,39 @@
 #! /bin/bash
-#we only need 24 tags for a chessboard equivalent grid, but we need more. if we're going to 
-#extend the grid beyond the image
-#GRIDFILE=/home/thompson/build/NifTK-qt4.8.7/AprilTags/src/tags/tag25h7.ps
-GRIDFILE=/home/thompson/work/build/NifTK-dbg/AprilTags/src/tags/tag25h7.ps
-OUTDIR=/dev/shm/AprilGridTemp
+APRILTAG_SRC=$1
+GRIDFILE=${APRILTAG_SRC}/tags/tag25h7.ps
+OUTDIR=./AprilGridsTemp
 mkdir -p $OUTDIR
 
-#we'll use imagemagick's convert and montage applications
+#check we have imagemagick for convert and montage
 
-if [ $GRIDFILE -nt $OUTDIR/000.bmp ]
+if [ $# -ne 1 ]
+then
+	echo "usage " $0 " pathToAprilTagSource"
+	echo "reads tag25h7.ps and creates a 6x4 and 18x12 grid of AprilTags"
+	exit
+fi
+
+
+if [ ! $(which convert 2> /dev/null) ]
+then
+	echo "ImageMagick not found on your path, please check installation"
+	exit
+fi
+
+if [ ! -e $GRIDFILE ]
+then
+	echo "Failed to find " $GRIDFILE
+	exit
+fi
+
+if [ ! -e $OUTDIR/000.bmp ] || [ $GRIDFILE -nt $OUTDIR/000.bmp ]
 then
 	convert $GRIDFILE $OUTDIR/%03d.bmp
 fi
 
 files=$(ls $OUTDIR/???.bmp)
 
-for file in $files 
+for file in $files
 do
 	if [ ${file} -nt ${file%.*}_cropped.bmp ]
 	then
@@ -27,6 +45,9 @@ done
 
 if [ ${OUTDIR}/000_cropped.bmp -nt montage24.bmp ]
 then
+
+	#we only need 24 tags for a chessboard equivalent grid, but we need more. if we're going to
+	#extend the grid beyond the image
 	files24=$(ls ${OUTDIR}/???_cropped.bmp | head -n 24)
 
 	montage $files24 -geometry 612x612+0+23 -tile 6x4 montage24.bmp
@@ -44,8 +65,8 @@ fi
 #tag corner to corner width = 612 * 6 - 68-2 = 3536 pixels. We want this equal 39 mm, same as chessboard,
 #pixTOmm=39  / 3536
 #image width in mm = 612*6 * 39/3536 = 40.50 mm
-#need to fiddle the montage setting to get 2 more mm into vertical size 
-#1 * 3536 / 39 = 90 pixels 
+#need to fiddle the montage setting to get 2 more mm into vertical size
+#1 * 3536 / 39 = 90 pixels
 #136/4 = 34 pixel per patch
 
 #13.02 percent scaling
