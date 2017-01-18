f837ac35a81d9d57abe7eae56829f3fa571ada6c
s.thompson@ucl.ac.uk <s.thompson@ucl.ac.uk>
2017-01-18 15:26:43 +0000

    Issue #39 increased border width on ring template images

--- Code/Bash/niftkMakeRingTemplates.bash
+++ Code/Bash/niftkMakeRingTemplates.bash
@@ -35,25 +35,28 @@ ycentre=$(echo ${SPACING_PIX}*${row}+${BORDER_PIX}/2 | bc )
 
 drawString=$(echo ${drawString} fill black circle $xcentre,$ycentre $xedge,$ycentre fill white circle $xcentre,$ycentre $xinneredge,$ycentre)
 	
-convert -size ${IMAGEWIDTH_PIX}x${IMAGEHEIGHT_PIX} xc:white  \
+convert -size ${IMAGEWIDTH_PIX}x${IMAGEHEIGHT_PIX} xc:white \
+	-depth 8 \
 	-draw "${drawString}" \
 	-blur 40x100 ringsTemplateImage.bmp
 
-#make the reference image and point positions
+#make the reference image and point positions, we need a bigger border so we don't fall off the edge
 
+BORDER=12.0
+BORDER_PIX=$(echo $BORDER*${PIXELSPERMM} | bc -l)
 INNERCOLS=$(($COLUMNS-1))
 INNERROWS=$(($ROWS-1))
 IMAGEWIDTH_PIX=$(echo ${INNERCOLS}*${SPACING_PIX}+${BORDER_PIX} | bc -l)
 IMAGEHEIGHT_PIX=$(echo ${INNERROWS}*${SPACING_PIX}+${BORDER_PIX} | bc -l)
 
 drawString=""
-row=0
+column=0
 rm ringsReferencePoints.txt
 pointIndex=0
-while [ $row -lt $ROWS ]
+while [ $column -lt $COLUMNS ]
 do
-	column=0
-	while [ $column -lt $COLUMNS ]
+	row=0
+	while [ $row -lt $ROWS ]
 	do	
 		xcentre=$(echo ${SPACING_PIX}*${column}+${BORDER_PIX}/2 | bc )
 		xedge=$(echo ${xcentre}+${OUTERDIAMETER_PIX}/2 | bc )
@@ -64,12 +67,13 @@ do
 		echo  $pointIndex $xcentre $ycentre >> ringsReferencePoints.txt
 		pointIndex=$(($pointIndex + 1 ))
 		drawString=$(echo ${drawString} fill black circle $xcentre,$ycentre $xedge,$ycentre fill white circle $xcentre,$ycentre $xinneredge,$ycentre)
-		column=$(($column+1))
+		row=$(($row+1))
 	done
-	row=$(($row+1))
+	column=$(($column+1))
 done
 
-convert -size ${IMAGEWIDTH_PIX}x${IMAGEHEIGHT_PIX} xc:white  \
+convert -size ${IMAGEWIDTH_PIX}x${IMAGEHEIGHT_PIX} xc:white \
+	-depth 8 \
 	-draw "${drawString}" \
 	-blur 40x100 ringsReferenceImage.bmp
 
