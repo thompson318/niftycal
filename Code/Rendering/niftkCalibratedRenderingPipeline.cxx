563220c0857310bf856752c79cad853c77bd8e4e
Matt Clarkson <m.clarkson@ucl.ac.uk>
2017-06-03 21:09:54 +0100

    Issue #52: Add calibrated model rendering pipeline.

---
+++ Code/Rendering/niftkCalibratedRenderingPipeline.cxx
@@ -0,0 +1,224 @@
+/*=============================================================================
+
+  NiftyCal: A software package for camera calibration.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#include "niftkCalibratedRenderingPipeline.h"
+#include <niftkNiftyCalExceptionMacro.h>
+#include <vtkWindowToImageFilter.h>
+#include <vtkPNGWriter.h>
+#include <vtkProperty.h>
+
+//-----------------------------------------------------------------------------
+CalibratedRenderingPipeline::CalibratedRenderingPipeline(
+    const cv::Vec2i&   windowSize,           // e.g. 1920x1080
+    const cv::Vec2i&   calibratedWindowSize, // normally, 1920x1080, but could be scaled such as 1920x540.
+    const std::string& modelFileName,
+    const std::string& textureFileName
+    )
+: m_WindowSize(windowSize)
+, m_CalibratedWindowSize(calibratedWindowSize)
+{
+  // Do all validation early, and bail out without doing anything.
+  if (windowSize[0] <= 0 || windowSize[1] <= 0)
+  {
+    niftkNiftyCalThrow() << "Invalid windowSize:" << windowSize;
+  }
+  if (calibratedWindowSize[0] <= 0 || calibratedWindowSize[1] <= 0)
+  {
+    niftkNiftyCalThrow() << "Invalid windowSize:" << windowSize;
+  }
+  if (modelFileName.empty())
+  {
+    niftkNiftyCalThrow() << "Model file name is empty.";
+  }
+  if (textureFileName.empty())
+  {
+    niftkNiftyCalThrow() << "Texture file name is empty.";
+  }
+
+  m_AspectRatio[0] = static_cast<double>(windowSize[0])/static_cast<double>(calibratedWindowSize[0]);
+  m_AspectRatio[1] = static_cast<double>(windowSize[1])/static_cast<double>(calibratedWindowSize[1]);
+
+  m_Camera = vtkSmartPointer<CalibratedCamera>::New();
+  m_Camera->SetActualWindowSize(windowSize[0], windowSize[1]);
+  m_Camera->SetCalibratedImageSize(calibratedWindowSize[0], calibratedWindowSize[1], m_AspectRatio[0]/m_AspectRatio[1]);
+  m_Camera->SetUseCalibratedCamera(true);
+
+  m_ModelReader = vtkSmartPointer<vtkPolyDataReader>::New();
+  m_ModelReader->SetFileName(modelFileName.c_str());
+
+  m_ModelToWorldMatrix = vtkSmartPointer<vtkMatrix4x4>::New();
+  m_ModelToWorldMatrix->Identity();
+
+  m_ModelToWorldTransform = vtkSmartPointer<vtkMatrixToLinearTransform>::New();
+  m_ModelToWorldTransform->SetInput(m_ModelToWorldMatrix);
+
+  m_ModelTransformFilter = vtkSmartPointer<vtkTransformPolyDataFilter>::New();
+  m_ModelTransformFilter->SetInputConnection(m_ModelReader->GetOutputPort());
+  m_ModelTransformFilter->SetTransform(m_ModelToWorldTransform);
+
+  m_TextureReader = vtkSmartPointer<vtkPNGReader>::New();
+  m_TextureReader->SetFileName(textureFileName.c_str());
+
+  m_Texture = vtkSmartPointer<vtkTexture>::New();
+  m_Texture->SetInputConnection(m_TextureReader->GetOutputPort());
+  m_Texture->InterpolateOff();
+
+  m_ModelMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+  m_ModelMapper->SetInputConnection(m_ModelTransformFilter->GetOutputPort());
+  m_ModelMapper->ScalarVisibilityOff();
+
+  m_ModelActor = vtkSmartPointer<vtkActor>::New();
+  m_ModelActor->GetProperty()->BackfaceCullingOn();
+  m_ModelActor->GetProperty()->SetInterpolationToFlat();
+
+  m_Renderer = vtkSmartPointer<vtkRenderer>::New();
+  m_Renderer->SetBackground(0, 0, 255);  // RGB
+  m_Renderer->AddActor(m_ModelActor);
+
+  m_Renderer->SetActiveCamera(m_Camera);
+  m_Renderer->SetLightFollowCamera(true);
+
+  m_RenderWin = vtkSmartPointer<vtkRenderWindow>::New();
+  m_RenderWin->AddRenderer(m_Renderer);
+
+  m_RenderWin->SetSize(m_WindowSize[0], m_WindowSize[1]);
+  m_RenderWin->DoubleBufferOff();
+  m_RenderWin->Render();
+
+  m_IntrinsicMatrix = cv::Mat::eye(3, 3, CV_64FC1);
+  m_LeftToRightMatrix = cv::Matx44d::eye();
+  m_RightToLeftMatrix = cv::Matx44d::eye();
+  m_CameraToWorldMatrix = cv::Matx44d::eye();
+  m_CameraMatrix = cv::Matx44d::eye();
+}
+
+
+//-----------------------------------------------------------------------------
+CalibratedRenderingPipeline::~CalibratedRenderingPipeline()
+{
+}
+
+
+//-----------------------------------------------------------------------------
+void CalibratedRenderingPipeline::OpenCVToVTK(const cv::Matx44d& openCV, vtkMatrix4x4& vtk)
+{
+  for (int r = 0; r < 4; r++)
+  {
+    for (int c = 0; c < 4; c++)
+    {
+      vtk.SetElement(r, c, openCV(r,c));
+    }
+  }
+}
+
+
+//-----------------------------------------------------------------------------
+vtkRenderWindow* CalibratedRenderingPipeline::GetRenderWindow() const
+{
+  return m_RenderWin;
+}
+
+
+//-----------------------------------------------------------------------------
+void CalibratedRenderingPipeline::Render()
+{
+  this->UpdateCamera();
+  m_Camera->Modified();
+  m_ModelActor->Modified();
+  m_Renderer->Modified();
+  m_RenderWin->Render();
+}
+
+
+//-----------------------------------------------------------------------------
+void CalibratedRenderingPipeline::DumpScreen(const std::string fileName)
+{
+  // Keep these local, or else the vtkWindowToImageFilter always appeared to cache its output,
+  // regardless of the value of ShouldRerenderOn.
+
+  vtkSmartPointer<vtkWindowToImageFilter> renderWindowToImageFilter = vtkSmartPointer<vtkWindowToImageFilter>::New();
+  renderWindowToImageFilter->SetInput(m_RenderWin);
+  renderWindowToImageFilter->SetInputBufferTypeToRGB();
+  renderWindowToImageFilter->SetMagnification(1);
+  renderWindowToImageFilter->ShouldRerenderOn();
+
+  vtkSmartPointer<vtkPNGWriter> renderedImageWriter = vtkSmartPointer<vtkPNGWriter>::New();
+  renderedImageWriter->SetInputConnection(renderWindowToImageFilter->GetOutputPort());
+  renderedImageWriter->SetFileName(fileName.c_str());
+  renderedImageWriter->Write();
+}
+
+
+//-----------------------------------------------------------------------------
+void CalibratedRenderingPipeline::SetIntrinsics(const cv::Mat& intrinsics)
+{
+  m_IntrinsicMatrix = intrinsics;
+}
+
+
+//-----------------------------------------------------------------------------
+void CalibratedRenderingPipeline::SetModelToWorldMatrix(const cv::Matx44d& modelToWorld)
+{
+  this->OpenCVToVTK(modelToWorld, *m_ModelToWorldMatrix);
+  m_ModelToWorldMatrix->Modified();
+  m_ModelToWorldTransform->Modified();
+}
+
+
+//-----------------------------------------------------------------------------
+void CalibratedRenderingPipeline::SetCameraToWorldMatrix(const cv::Matx44d& cameraToWorld)
+{
+  m_CameraToWorldMatrix = cameraToWorld;
+}
+
+
+//-----------------------------------------------------------------------------
+void CalibratedRenderingPipeline::SetLeftToRightMatrix(const cv::Matx44d& leftToRight)
+{
+  m_LeftToRightMatrix = leftToRight;
+  m_RightToLeftMatrix = leftToRight.inv();
+}
+
+
+//-----------------------------------------------------------------------------
+void CalibratedRenderingPipeline::UpdateCamera()
+{
+  double origin[4]     = {0, 0,    0,    1};
+  double focalPoint[4] = {0, 0,   1000, 1};
+  double viewUp[4]     = {0, -1000, 0,    1};
+
+  m_Camera->SetIntrinsicParameters(m_IntrinsicMatrix.at<double>(0,0),
+                                   m_IntrinsicMatrix.at<double>(1,1),
+                                   m_IntrinsicMatrix.at<double>(0,2),
+                                   m_IntrinsicMatrix.at<double>(1,2));
+
+  m_CameraMatrix = m_CameraToWorldMatrix * m_RightToLeftMatrix;
+
+  vtkSmartPointer<vtkMatrix4x4> tmp = vtkSmartPointer<vtkMatrix4x4>::New();
+  this->OpenCVToVTK(m_CameraMatrix, *tmp);
+
+  tmp->MultiplyPoint(origin, origin);
+  tmp->MultiplyPoint(focalPoint, focalPoint);
+  tmp->MultiplyPoint(viewUp, viewUp);
+
+  viewUp[0] = viewUp[0] - origin[0];
+  viewUp[1] = viewUp[1] - origin[1];
+  viewUp[2] = viewUp[2] - origin[2];
+
+  m_Camera->SetPosition(origin[0], origin[1], origin[2]);
+  m_Camera->SetFocalPoint(focalPoint[0], focalPoint[1], focalPoint[2]);
+  m_Camera->SetViewUp(viewUp[0], viewUp[1], viewUp[2]);
+  m_Camera->SetClippingRange(1, 10000);
+  m_Camera->Modified();
+}
---
+++ Code/Rendering/niftkCalibratedRenderingPipeline.h
@@ -0,0 +1,127 @@
+/*=============================================================================
+
+  NiftyCal: A software package for camera calibration.
+
+  Copyright (c) University College London (UCL). All rights reserved.
+
+  This software is distributed WITHOUT ANY WARRANTY; without even
+  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+  PURPOSE.
+
+  See LICENSE.txt in the top level directory for details.
+
+=============================================================================*/
+
+#ifndef niftkCalibratedRenderingPipeline_h
+#define niftkCalibratedRenderingPipeline_h
+
+#include "niftkWin32ExportHeader.h"
+#include "niftkCalibratedCamera.h"
+
+#include <vtkObject.h>
+#include <vtkSmartPointer.h>
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkPolyDataReader.h>
+#include <vtkPolyDataMapper.h>
+#include <vtkTransformPolyDataFilter.h>
+#include <vtkMatrixToLinearTransform.h>
+#include <vtkPNGReader.h>
+#include <vtkTexture.h>
+
+#include <cv.h>
+
+/**
+ * \class CalibratedRenderingPipeline
+ * \brief Generates a texture (e.g. calibration image) onto
+ * a vtkPolyData model, and renders it with a calibrated camera.
+ */
+class NIFTYCAL_WINEXPORT CalibratedRenderingPipeline {
+
+public:
+
+  CalibratedRenderingPipeline(
+    const cv::Vec2i&   windowSize,           // e.g. 1920x1080
+    const cv::Vec2i&   calibratedWindowSize, // normally, 1920x1080, but could be scaled such as 1920x540.
+    const std::string& modelFileName,
+    const std::string& textureFileName
+  );
+
+  virtual ~CalibratedRenderingPipeline();
+
+  /**
+  * \brief Updates the whole VTK pipeline.
+  */
+  virtual void Render();
+
+  /**
+  * \brief Calls Render() and writes to specified file.
+  * \param fileName file name
+  */
+  void DumpScreen(const std::string fileName);
+
+  /**
+  * \brief Sets the intrinsic parameters on the camera.
+  */
+  void SetIntrinsics(const cv::Mat& intrinsics);
+
+  /**
+  * \brief Multiplies model vtkPolyData points by modelToWorld to give a position
+  * in world coordinates, having the effect of moving the model relative to the world coordinates.
+  * \param modelToWorld rigid body transform
+  */
+  void SetModelToWorldMatrix(const cv::Matx44d& modelToWorld);
+
+  /**
+  * \brief Used to set the camera position in world coordinates.
+  * \param cameraToWorld rigid body transform
+  */
+  void SetCameraToWorldMatrix(const cv::Matx44d& modelToWorld);
+
+  /**
+   * \brief Defaults to Identity.
+   * \param leftToRight rigid body transform
+   */
+  void SetLeftToRightMatrix(const cv::Matx44d& leftToRight);
+
+  /**
+  * \brief Returns a pointer to the vtkRenderWindow.
+  */
+  vtkRenderWindow* GetRenderWindow() const;
+
+private:
+
+  CalibratedRenderingPipeline(const CalibratedRenderingPipeline&);  // Purposefully not implemented.
+  void operator=(const CalibratedRenderingPipeline&);  // Purposefully not implemented.
+
+  void UpdateCamera();
+  void OpenCVToVTK(const cv::Matx44d& openCV, vtkMatrix4x4& vtk);
+
+  cv::Vec2i                                    m_WindowSize;
+  cv::Vec2i                                    m_CalibratedWindowSize;
+  cv::Vec2d                                    m_AspectRatio;
+
+  cv::Mat                                      m_IntrinsicMatrix;
+  cv::Matx44d                                  m_LeftToRightMatrix;
+  cv::Matx44d                                  m_RightToLeftMatrix;
+  cv::Matx44d                                  m_CameraToWorldMatrix;
+  cv::Matx44d                                  m_CameraMatrix;
+
+  vtkSmartPointer<CalibratedCamera>            m_Camera;
+
+  vtkSmartPointer<vtkMatrix4x4>                m_ModelToWorldMatrix;
+  vtkSmartPointer<vtkMatrixToLinearTransform>  m_ModelToWorldTransform;
+
+  vtkSmartPointer<vtkPNGReader>                m_TextureReader;
+  vtkSmartPointer<vtkTexture>                  m_Texture;
+
+  vtkSmartPointer<vtkPolyDataReader>           m_ModelReader;
+  vtkSmartPointer<vtkTransformPolyDataFilter>  m_ModelTransformFilter;
+  vtkSmartPointer<vtkPolyDataMapper>           m_ModelMapper;
+  vtkSmartPointer<vtkActor>                    m_ModelActor;
+
+  vtkSmartPointer<vtkRenderer>                 m_Renderer;
+  vtkSmartPointer<vtkRenderWindow>             m_RenderWin;
+};
+
+#endif
