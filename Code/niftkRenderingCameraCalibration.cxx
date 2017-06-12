bee873231b13861dc01e46831d455899d6deaa6b
Matt Clarkson <m.clarkson@ucl.ac.uk>
2017-06-12 20:49:57 +0100

    Issue #52: Simplify Rendering Calibration interface.

--- Code/niftkRenderingCameraCalibration.cxx
+++ Code/niftkRenderingCameraCalibration.cxx
@@ -18,20 +18,16 @@ namespace niftk
 {
 
 //-----------------------------------------------------------------------------
-double RenderingMonoCameraCalibration(const niftk::Model3D& model3D,
-                                      const niftk::PointSet& imagePoints2D,
-                                      const std::string& modelFileName,
-                                      const std::string& textureFileName,
-                                      const std::vector<cv::Mat>& images,
-                                      const cv::Size2i& imageSize,
-                                      cv::Mat& intrinsic,
-                                      cv::Mat& distortion,
-                                      std::vector<cv::Mat>& rvecs,
-                                      std::vector<cv::Mat>& tvecs
-                                     )
+void RenderingMonoCameraCalibration(const std::string& modelFileName,
+                                    const std::string& textureFileName,
+                                    const std::vector<cv::Mat>& images,
+                                    const cv::Size2i& imageSize,
+                                    cv::Mat& intrinsic,
+                                    cv::Mat& distortion,
+                                    std::vector<cv::Mat>& rvecs,
+                                    std::vector<cv::Mat>& tvecs
+                                   )
 {
-  double rms = 0;
-
   // Algorithm
   // While still improving.
   //   1. Optimise intrinsic/distortion, as you render ONCE at each pose,
@@ -39,41 +35,32 @@ double RenderingMonoCameraCalibration(const niftk::Model3D& model3D,
   //   2. Optimise extrinsic, as you unwarp once for each image,
   //   then repeatedly render the model to match the images.
   // end
-
-  return rms;
 }
 
 
 //-----------------------------------------------------------------------------
-cv::Matx21d RenderingStereoCameraCalibration(const niftk::Model3D& model3D,
-                                             const niftk::PointSet& points2DLeft,
-                                             const niftk::PointSet& points2DRight,
-                                             const std::string& modelFileName,
-                                             const std::string& textureFileName,
-                                             const std::vector<PointSet>& leftImages,
-                                             const std::vector<PointSet>& rightImages,
-                                             cv::Mat& intrinsicLeft,
-                                             cv::Mat& distortionLeft,
-                                             std::vector<cv::Mat>& rvecsLeft,
-                                             std::vector<cv::Mat>& tvecsLeft,
-                                             cv::Mat& intrinsicRight,
-                                             cv::Mat& distortionRight,
-                                             std::vector<cv::Mat>& rvecsRight,
-                                             std::vector<cv::Mat>& tvecsRight,
-                                             cv::Mat& leftToRightRotationMatrix,
-                                             cv::Mat& leftToRightTranslationVector
-                                            )
+void RenderingStereoCameraCalibration(const std::string& modelFileName,
+                                      const std::string& textureFileName,
+                                      const std::vector<PointSet>& leftImages,
+                                      const std::vector<PointSet>& rightImages,
+                                      cv::Mat& intrinsicLeft,
+                                      cv::Mat& distortionLeft,
+                                      std::vector<cv::Mat>& rvecsLeft,
+                                      std::vector<cv::Mat>& tvecsLeft,
+                                      cv::Mat& intrinsicRight,
+                                      cv::Mat& distortionRight,
+                                      std::vector<cv::Mat>& rvecsRight,
+                                      std::vector<cv::Mat>& tvecsRight,
+                                      cv::Mat& leftToRightRotationMatrix,
+                                      cv::Mat& leftToRightTranslationVector
+                                     )
 {
-  cv::Matx21d rms;
-
   // Algorithm
   // While still improving.
   //   1. Optimise intrinsic/distortion for left camera
   //   2. Optimise intrinsic/distortion for right camera
   //   3. Optimise extrinsic for left and right simultanously.
   // end
-
-  return rms;
 }
 
 } // end namespace
--- Code/niftkRenderingCameraCalibration.h
+++ Code/niftkRenderingCameraCalibration.h
@@ -31,42 +31,35 @@ namespace niftk
 
 /**
 * \brief Performs a mono camera calibration using rendering of a model.
-* \return rms re-projection error.
 */
-NIFTYCAL_WINEXPORT double RenderingMonoCameraCalibration(const niftk::Model3D& model3D,
-                                                         const niftk::PointSet& imagePoints2D,
-                                                         const std::string& modelFileName,
-                                                         const std::string& textureFileName,
-                                                         const std::vector<cv::Mat>& images,
-                                                         const cv::Size2i& imageSize,
-                                                         cv::Mat& intrinsic,
-                                                         cv::Mat& distortion,
-                                                         std::vector<cv::Mat>& rvecs,
-                                                         std::vector<cv::Mat>& tvecs
-                                                        );
+NIFTYCAL_WINEXPORT void RenderingMonoCameraCalibration(const std::string& modelFileName,
+                                                       const std::string& textureFileName,
+                                                       const std::vector<cv::Mat>& images,
+                                                       const cv::Size2i& imageSize,
+                                                       cv::Mat& intrinsic,
+                                                       cv::Mat& distortion,
+                                                       std::vector<cv::Mat>& rvecs,
+                                                       std::vector<cv::Mat>& tvecs
+                                                      );
 
 /**
 * \brief Performs a stereo camera calibration using rendering of a model.
-* \return rms re-projection error (pix) and re-construction error (mm).
 */
-NIFTYCAL_WINEXPORT cv::Matx21d RenderingStereoCameraCalibration(const niftk::Model3D& model3D,
-                                                                const niftk::PointSet& points2DLeft,
-                                                                const niftk::PointSet& points2DRight,
-                                                                const std::string& modelFileName,
-                                                                const std::string& textureFileName,
-                                                                const std::vector<PointSet>& leftImages,
-                                                                const std::vector<PointSet>& rightImages,
-                                                                cv::Mat& intrinsicLeft,
-                                                                cv::Mat& distortionLeft,
-                                                                std::vector<cv::Mat>& rvecsLeft,
-                                                                std::vector<cv::Mat>& tvecsLeft,
-                                                                cv::Mat& intrinsicRight,
-                                                                cv::Mat& distortionRight,
-                                                                std::vector<cv::Mat>& rvecsRight,
-                                                                std::vector<cv::Mat>& tvecsRight,
-                                                                cv::Mat& leftToRightRotationMatrix,
-                                                                cv::Mat& leftToRightTranslationVector
-                                                               );
+NIFTYCAL_WINEXPORT void RenderingStereoCameraCalibration(const std::string& modelFileName,
+                                                         const std::string& textureFileName,
+                                                         const std::vector<PointSet>& leftImages,
+                                                         const std::vector<PointSet>& rightImages,
+                                                         cv::Mat& intrinsicLeft,
+                                                         cv::Mat& distortionLeft,
+                                                         std::vector<cv::Mat>& rvecsLeft,
+                                                         std::vector<cv::Mat>& tvecsLeft,
+                                                         cv::Mat& intrinsicRight,
+                                                         cv::Mat& distortionRight,
+                                                         std::vector<cv::Mat>& rvecsRight,
+                                                         std::vector<cv::Mat>& tvecsRight,
+                                                         cv::Mat& leftToRightRotationMatrix,
+                                                         cv::Mat& leftToRightTranslationVector
+                                                        );
 } // end namespace
 
 #endif
